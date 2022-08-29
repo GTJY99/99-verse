bc3666273d6c3a0612be2018d4ec153363b8dccc
GTJY99 <tangjy1999@gmail.com>
2022-08-29 23:25:56 +0800

    gallery and about us tab

---
+++ About_us/About_us.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function opensidebar() {
+    document.getElementById("SIDEBAR").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closesidebar() {
+    document.getElementById("SIDEBAR").style.width = "0";
+  }
