ddaddd087ca58fa3ea7cb5f13e4657a6dd892738
GTJY99 <tangjy1999@gmail.com>
2022-09-01 21:58:28 +0800

    File arrangement into folder

--- function.js
+++
@@ -1,8 +0,0 @@
-//open navigation panel
-function openNav() {
-    document.getElementById("navigationPanel").style.width = "250px";
-}
-//close navigation panel
-function closeNav() {
-    document.getElementById("navigationPanel").style.width = "0";
-}
