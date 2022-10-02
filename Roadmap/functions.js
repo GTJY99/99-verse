1081271b75d44618dc4c73681d3560aa8f129b78
jie1118 <700024209@student.curtin.edu.my>
2022-10-02 12:01:35 +0800

    add switch case

--- Roadmap/functions.js
+++ Roadmap/functions.js
@@ -1,41 +1,20 @@
-var pointer = document.getElementById("myPointer")
-var output = document.getElementById("myValue")
-output.innerHTML = pointer.value;
 
-pointer.oninput = function() {
-    output.innerHTML = this.value;
-    filterEvent(this.value);
-}
+var casenumber = document.getElementById("myPointer").value;
 
-
-
-//functions from filter
-function filterEvent(pointervalue) {
-    var x, i;
-    x = document.getElementsByClassName("event");
-    for (i = 0; i < x.length; i++){
-        eventRemove(x[i], "show");
-        if (x[i].className.indexOf(pointervalue) > -1) eventAdd(x[i], "show");
+//switch case to identify event
+  function showEvent() {
+    var timeline;
+    switch (casenumber) {
+      case "1":
+        timeline = "On this day, this group is formed..."
+        break;
+      case "2":
+        timeline = "I forgot what has happened haha"
+        break;
+      default:
+        timeline = "nothing"
     }
-}
-
-function eventAdd(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    return timeline;
   }
-}
+  document.getElementById("event").innerHTML = showEvent();
 
-function eventRemove(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
-    }
-  }
-  element.className = arr1.join(" ");
-}
